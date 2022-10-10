9ded8cbe8f2e2e406bb69c80d819b7e507c14ecc
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2022-10-10 17:00:41 +0200

    Players added

---
+++ src/Player.svelte
@@ -0,0 +1,36 @@
+<script>
+    let name = "John Doe";
+	let points = 100;
+	let showControls = false;
+
+	const addPoint = () => points += 1;
+	const removePoint = () => points -= 1;
+	const toggleControls = () => (showControls = !showControls);
+</script>
+
+<style>
+	h1 {
+		color: #204f6e;
+	}
+
+	h3 {
+		margin-bottom: 10px;
+	}
+</style>
+
+<div class="container">
+	<div class="card">
+		<h1>
+			{name}
+			<button class="btn btn-sm" on:click={toggleControls}>
+				{#if showControls}-{:else}+{/if}
+			</button>
+		</h1>
+		<h3>Points: {points}</h3>
+		{#if showControls}
+		<div class="btn" on:click={addPoint}>+1</div>
+		<div class="btn btn-dark" on:click={removePoint}>-1</div>
+		<input type="number" bind:value={points}>
+		{/if}
+	</div>
+</div>
