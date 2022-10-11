23762862dd06168158cf6f87bc346ce961fb9324
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2022-10-11 12:50:50 +0200

    adding and removing Player added - project complet

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,36 +1,40 @@
 <script>
-    export let name;
+	import { createEventDispatcher } from "svelte";
+  
+	const dispatch = createEventDispatcher();
+  
+	export let name;
 	export let points;
 	let showControls = false;
-
-	const addPoint = () => points += 1;
-	const removePoint = () => points -= 1;
+  
+	const addPoint = () => (points += 1);
+	const removePoint = () => (points -= 1);
 	const toggleControls = () => (showControls = !showControls);
-</script>
-
-<style>
+	const onDelete = () => dispatch("removeplayer", name);
+  </script>
+  
+  <style>
 	h1 {
-		color: #204f6e;
+	  color: #204f6e;
 	}
-
+  
 	h3 {
-		margin-bottom: 10px;
+	  margin-bottom: 10px;
 	}
-</style>
-
-<div class="container">
-	<div class="card">
-		<h1>
-			{name}
-			<button class="btn btn-sm" on:click={toggleControls}>
-				{#if showControls}-{:else}+{/if}
-			</button>
-		</h1>
-		<h3>Points: {points}</h3>
-		{#if showControls}
-		<div class="btn" on:click={addPoint}>+1</div>
-		<div class="btn btn-dark" on:click={removePoint}>-1</div>
-		<input type="number" bind:value={points}>
-		{/if}
-	</div>
-</div>
+  </style>
+  
+  <div class="card">
+	<h1>
+	   {name}
+	  <button class="btn btn-sm" on:click={toggleControls}>
+		{#if showControls}-{:else}+{/if}
+	  </button>
+	  <button class="btn btn-danger btn-sm" on:click={onDelete}>x</button>
+	</h1>
+	<h3>Points: {points}</h3>
+	{#if showControls}
+	  <button class="btn" on:click={addPoint}>+1</button>
+	  <button class="btn btn-dark" on:click={removePoint}>-1</button>
+	  <input type="number" bind:value={points} />
+	{/if}
+  </div>
