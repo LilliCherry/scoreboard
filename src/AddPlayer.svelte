6a44fa98e0243e716c7a8a2a53b7321e40a5794a
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2022-10-11 11:29:26 +0200

    player adding feature wip 2

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -1,8 +1,21 @@
 <script>
-    let player = {
-        name:"",
-        points: 0
-    }
+      import { createEventDispatcher } from "svelte";
+
+const dispatch = createEventDispatcher();
+
+let player = {
+  name: "",
+  points: 0
+};
+
+const onSubmit = e => {
+  e.preventDefault();
+  dispatch("addplayer", player);
+  player = {
+    name: "",
+    points: 0
+  };
+};
 </script>
 
 <form class="grid-3">
